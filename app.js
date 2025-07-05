cce2b5d2b42cc951b27789c7cc1ca7db25253f8c
WodaMineralna <raknieborak65@gmail.com>
2025-07-05 16:19:43 +0200

    Sending a first Response to the client

--- app.js
+++ app.js
@@ -1,7 +1,14 @@
 const http = require("http");
 
 const server = http.createServer((req, res) => {
-  console.log(req.url, req.method, req.headers) // the most important fields
-})
+  console.log(req.url, req.method, req.headers); // the most important fields
 
-server.listen(3000)
+  res.setHeader("Content-Type", "text/html"); // * here we specify that we we pass HTML Code as a response
+  res.write("<html>");
+  res.write("<head><title>My First NodeJS Page!</title></head>");
+  res.write("<body><h1>Hello from my Node.js Server!</h1></body>");
+  res.write("</html>");
+  res.end();
+});
+
+server.listen(3000);
